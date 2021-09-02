bfa0651fb85a66857670f17a3133c734c3405250
Dheenadhayalan R <dheenadhayalanr19@gmail.com>
2021-09-02 23:14:05 +0530

    Make frame to combine every other frame

--- float_chat/GUI/ChatBox.py
+++ float_chat/GUI/ChatBox.py
@@ -23,9 +23,8 @@ class Chat(tk.Frame):
         username = tk.StringVar()  # label holding username
         content = tk.StringVar()  # label holding content
 
-        user_font = ('calibri', 11, 'bold')  # font specifications
-        content_font = ('calibri', 11, 'normal')
-        tip_font = ('calibri', 10, 'normal')
+        user_font = ('calibri', 10, 'bold')  # font specifications
+        content_font = ('calibri', 10, 'normal')
         username_label = tk.Label(self, textvariable=username, anchor=tk.W)  # create label widgets
         content_label = tk.Label(self, textvariable=content)
 
@@ -40,7 +39,7 @@ class Chat(tk.Frame):
 
 
 class ChatArray(tk.Frame):
-    def __init__(self, parent=None, total_chats=10, **kwargs):
+    def __init__(self, parent=None, **kwargs):
         """
         Initializing necessary values
         :param parent: parent frame
@@ -49,33 +48,79 @@ class ChatArray(tk.Frame):
         """
         super().__init__(parent, **kwargs)
         self.parent = parent
-        self.total_chat = total_chats
         self.chats = None  # list which will contain Chat cells
+        self.is_created = False  # avoid multiple calls to create_widget method
 
-    def create_widgets(self):
+    def create_widgets(self, total_chats=10):
         """
         Creates and arrange Chat cells
         :return: tuple of chat cell data variables
         """
+        if self.is_created:  # proceed only if this is the first call
+            return
         chats = list()  # holds chat data
-        for i in range(self.total_chat):
+        for i in range(total_chats):
             chat_cell = Chat(self)  # create chat cells
             chat = chat_cell.create_widgets()
             chat_cell.grid(row=i, column=0)  # arrange chat cells
             chats.append(chat)
-        self.chats = tuple(chats)  # creating immutable order
+        self.chats = tuple(chats[::-1])  # Arrange chat in bottom first order
+        self.is_created = True
 
         return self.chats
 
 
+class ChatBox(tk.Frame):
+    """
+    This frame combine all the other frames to create whole chat box
+    """
+    def __init__(self, parent=None, **kwargs):
+        """
+        Initializing necessary values
+        :param parent: parent frame
+        :param total_chats: number of Chat frames the ChatArray should have
+        :param kwargs: extra parameters for tkinter Frame
+        """
+        super().__init__(parent, **kwargs)
+        self.canvas = tk.Canvas(self)  # to contain ChatArray
+        self.chat_array = ChatArray(self.canvas)
+        self.chat_data = self.chat_array.create_widgets()
+
+        self.chat_array.update()
+        chat_array_width = self.chat_array.winfo_reqwidth()
+        self.canvas.config(width=chat_array_width, height=300)  # fixing size of canvas
+        self.fit_canvas()
+        self.canvas.config(highlightthickness=0)
+        self.scrollbar = tk.Scrollbar(self)
+
+        # configuring canvas and scrollbar
+        self.scrollbar.config(command=self.canvas.yview)
+        self.canvas.config(yscrollcommand=self.scrollbar.set)
+
+        # arranging widgets
+        self.canvas.create_window(0, 0, anchor=tk.NW, window=self.chat_array)
+
+        self.canvas.grid(row=0, column=0)
+        self.scrollbar.grid(row=0, column=1, sticky=tk.NS)
+
+    def update_callback(self, chat_data):
+        for i in range(len(chat_data)):
+            username, content = chat_data[i]
+            rev_username = username[:28] + '...' if len(username) > 30 else username  # add continue dots
+            username_var, content_var = self.chat_data[i]
+            username_var.set(rev_username)
+            content_var.set(content)
+        self.fit_canvas()
+
+    def fit_canvas(self):
+        self.chat_array.update()
+        chat_array_width = self.chat_array.winfo_reqwidth()
+        chat_array_height = self.chat_array.winfo_reqheight()
+        self.canvas.config(scrollregion=(0, 0, chat_array_width, chat_array_height))
+
+
 if __name__ == '__main__':
     root = tk.Tk()
-    widget = ChatArray(root)
-    widget.pack(expand=tk.YES, fill=tk.BOTH)
-    message = 'The len() Python method returns the length of a list, string, dictionary, or any other iterable data format in Python. The len() method takes one argument: an iterable object. ... The Python len() method is a built-in function that can be used to calculate the length of any iterable object.'
-    user = '2019BCS0038 Dheenadhayalan Ramadoss'
-    chats = widget.create_widgets()
-    '''for username, content in chats:
-        username.set(user)
-        content.set(message+'\n')'''
+    widget = ChatBox(root)
+    widget.pack()
     root.mainloop()
