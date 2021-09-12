de187be9e0f761316c3eb8655d0b355f3d7be263
Dheenadhayalan R <dheenadhayalanr19@gmail.com>
2021-09-12 10:22:13 +0530

    Update comments

--- float_chat/GUI/MainMenu.py
+++ float_chat/GUI/MainMenu.py
@@ -4,49 +4,79 @@ from . import ChatBox
 
 
 class MainMenu(tk.Frame):
+    """
+    Frame of opening GUI facilitates float chat toggle actions
+    """
     def __init__(self, parent=None, **kwargs):
+        """
+        Initializing necessary values
+        :param parent: parent frame
+        :param kwargs: extra parameters for tkinter Frame
+        """
         super().__init__(parent, **kwargs)
         self.parent = parent
 
-        buttons = dict()
-        buttons['float_chat'] = tk.Button(self, text='Start float chat', command=self.toggle_chat)
-        buttons['quit'] = tk.Button(self, text='Quit', command=self.if_quit)
+        buttons = dict()  # contains the main buttons
+        buttons['float_chat'] = tk.Button(self, text='Start float chat', command=self.toggle_chat)  # toggles float chat
+        buttons['quit'] = tk.Button(self, text='Quit', command=self.if_quit)  # quit button
         self.buttons = buttons
 
+        # arrange buttons
         i = 0
         for x in buttons:
             buttons[x].grid(row=0, column=i)
             i += 1
 
-        self.web_handler = None
-        self.float_chat_on = False
-        self.float_chat_toplevel = None
+        self.web_handler = None  # web handler of the program
+        self.float_chat_on = False  # if float chat window is up
+        self.float_chat_toplevel = None  # toplevel of float chat window
 
     def connect_browser_handler(self, wh):
+        """
+        Sets the web handler from browserHandler.browserHandler.WebHandler
+        :param wh: created WebHandler object
+        :return: None
+        """
         self.web_handler = wh
 
     def toggle_chat(self):
+        """
+        Opens float chat if not available
+        Closes float chat if already opened
+        :return: None
+        """
         if not self.float_chat_on:
-            self.float_chat_toplevel = tk.Toplevel(self)
+            self.float_chat_toplevel = tk.Toplevel(self)  # toplevel for float chat
             self.float_chat_toplevel.protocol('WM_DELETE_WINDOW', self.on_float_chat_close)
-            self.float_chat_toplevel.attributes('-topmost', True)
+            self.float_chat_toplevel.attributes('-topmost', True)  # setting float chat top most
+
+            # create and align chat box
             chat_box = ChatBox.ChatBox(self.float_chat_toplevel)
             chat_box.pack(expand=tk.YES, fill=tk.Y)
             chat_box.align_window()
             self.float_chat_toplevel.resizable(False, True)
             self.web_handler.listen_chat(chat_box.update_callback)
-            self.buttons['float_chat'].config(text='Stop float chat')
-            self.float_chat_on = True
+
+            self.buttons['float_chat'].config(text='Stop float chat')  # change button text
+            self.float_chat_on = True  # change float chat on status
         else:
             self.on_float_chat_close()
 
     def on_float_chat_close(self):
-        self.web_handler.stop_listening()
-        self.float_chat_toplevel.destroy()
-        self.buttons['float_chat'].config(text='Start float chat')
-        self.float_chat_on = False
+        """
+        Called when float chat is to be closed
+        :return: None
+        """
+        self.web_handler.stop_listening()  # stops web handler from listening
+        self.float_chat_toplevel.destroy()  # close float chat
+        self.buttons['float_chat'].config(text='Start float chat')  # change button text
+        self.float_chat_on = False  # change float chat on status
 
     def if_quit(self):
+        """
+        When the main menu is to be quit
+        :return: None
+        """
         answer = helper.quit_popup()
         if answer:
             self.parent.destroy()
