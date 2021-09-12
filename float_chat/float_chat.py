c2c0c9630f70fee7a3d5e117a20e02ba9aefc052
Dheenadhayalan R <dheenadhayalanr19@gmail.com>
2021-09-12 11:18:43 +0530

    Create main application class

--- float_chat/float_chat.py
+++ float_chat/float_chat.py
@@ -4,27 +4,37 @@ from GUI import MainMenu
 from GUI import helper
 
 
-def main():
-    """
-    Combines the created modules and create the main application
-    :return: None
-    """
-    web_handler = browserHandler.WebHandler()  # open browser
+class Application:
+    def __init__(self):
+        """
+        Combines the created modules and create the main application
+        :return: None
+        """
+        web_handler = browserHandler.WebHandler()  # open browser
 
-    # create and configure root window
-    root = tk.Tk()
-    root.attributes('-topmost', True)
-    root.protocol('WM_DELETE_WINDOW', helper.quit_popup)
+        # create and configure root window
+        root = tk.Tk()
+        root.attributes('-topmost', True)
+        root.protocol('WM_DELETE_WINDOW', self.at_close)
 
-    # create and configure main menu
-    main_menu = MainMenu.MainMenu(root)
-    main_menu.pack()
-    main_menu.align_window()
-    main_menu.connect_browser_handler(web_handler)
+        # create and configure main menu
+        main_menu = MainMenu.MainMenu(root)
+        main_menu.pack()
+        main_menu.align_window()
+        main_menu.connect_browser_handler(web_handler)
 
-    root.resizable(False, False)  # root is not resizable
-    root.mainloop()
+        root.resizable(False, False)  # root is not resizable
+        self.root = root  # to facilitate closing
+        root.mainloop()
+
+    def at_close(self):
+        """
+        Called when close button is pressed
+        :return: None
+        """
+        if helper.quit_popup():  # if quit is confirmed
+            self.root.destroy()
 
 
 if __name__ == '__main__':
-    main()
+    Application()
